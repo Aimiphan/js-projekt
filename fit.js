999a61fd0fb756ad1c8f35adc3b5a09f1df47063
Aimiphan <aimiphan11@gmail.com>
2023-02-21 11:11:08 +0100

    Update fit.js

--- fit.js
+++ fit.js
@@ -2,26 +2,26 @@
 //Countdown
 var countDownDate = new Date("Jun 1, 2023 00:00:00").getTime();
 
-// Update the count down every 1 second
+// Nedtællingen bliver opdateret hvert sekund
 var x = setInterval(function() {
 
-  // Get today's date and time
+  // Dato og tid for i dag
   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
+  // Afstanden mellem nu og nedtællingsdatoen
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
+  // Tidsberegninger for dage, timer, minutter og sekunder
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in an element with id="demo"
+  // Udskriv resultatet i et element med id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
-  // If the count down is over, write some text
+  // Når countdown er ovre, vil der står dette...
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
