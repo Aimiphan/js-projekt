87ede716ee338cb40379290aaf156475008736d4
Aimiphan <aimiphan11@gmail.com>
2023-02-22 12:52:48 +0100

    slettet billede

--- fit.js
+++ fit.js
@@ -46,21 +46,3 @@ dropdownList.addEventListener('mouseout', () => {
 });
 
 //billedegalleri
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Endre tallet her for å justere tid mellom hvert bilde
-}
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
