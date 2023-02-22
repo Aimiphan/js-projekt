dad4328b0b3b854e5cbad84514dadb84082120d6
Aimiphan <aimiphan11@gmail.com>
2023-02-22 14:07:45 +0100

    .

--- fit.js
+++ fit.js
@@ -48,17 +48,3 @@ dropdownList.addEventListener('mouseout', () => {
 });
 
 //billedegalleri
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("galleri");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
