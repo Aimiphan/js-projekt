fcce24c4f083ae9ee4fab0fca6c0f6b89d75eab4
Aimiphan <aimiphan11@gmail.com>
2023-02-22 13:54:20 +0100

    galleri

--- fit.js
+++ fit.js
@@ -48,29 +48,22 @@ dropdownList.addEventListener('mouseout', () => {
 });
 
 //billedegalleri
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("galleri");
-  let dots = document.getElementsByClassName("galleri");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let slides = document.getElementsByClassName("mySlidesfade");
+  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
