956670c236cb855ce973450023277ccc91e2c0d8
annafoverskov <119939347+annafoverskov@users.noreply.github.com>
2023-02-23 10:16:21 +0100

    .

--- dropdown.js
+++ dropdown.js
@@ -1,18 +1,31 @@
-´function funktion1() {
-    document.getElementById("minDropdown").classList.toggle("show");
+// Get the dropdown menu element
+const dropdownMenu = document.getElementById("dropdown-menu");
+
+// Define an array of options
+const options = [
+  { value: "", text: "Select an option" },
+  { value: "option1", text: "Option 1" },
+  { value: "option2", text: "Option 2" },
+  { value: "option3", text: "Option 3" }
+];
+
+// Loop through the options array and create option elements
+options.forEach(option => {
+  const optionElement = document.createElement("option");
+  optionElement.value = option.value;
+  optionElement.text = option.text;
+  dropdownMenu.appendChild(optionElement);
+});
+
+// Define an event listener for the dropdown menu
+dropdownMenu.addEventListener("change", event => {
+  const selectedValue = event.target.value;
+  if (selectedValue === "option1") {
+    // Do something for option 1
+  } else if (selectedValue === "option2") {
+    // Do something for option 2
+  } else if (selectedValue === "option3") {
+    // Do something for option 3
   }
-  function filterFunction() {
-    let input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("minDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-      txtValue = a[i].textContent || a[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
-    }
-  }
+});
+
